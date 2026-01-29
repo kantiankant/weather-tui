0da5deb266f6d502844be3be7b2f7f8f35fce756
kantiankant <qwerasdnoob@gmail.com>
2026-01-29 16:39:46 +0800

    oops a daisy

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-echo "Building weather-searcher..."
-cargo build --release
-
-echo "Installing to /usr/local/bin..."
-sudo cp target/release/weather-searcher /usr/local/bin/weather-searcher
-
-echo "✓ Updated successfully!"
-echo "Run 'weather-searcher' to test"
