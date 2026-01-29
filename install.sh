fed2d039ec89b8271e113c74fb456b9bfdb7bcaf
kantiankant <qwerasdnoob@gmail.com>
2026-01-29 16:52:44 +0800

    Added an installer script because it makes life easy

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+# Check for cargo
+if ! command -v cargo >/dev/null 2>&1; then
+  echo "You muppet, you didn't even install Rust/Cargo and you actually expected it to compile?"
+  exit 1
+fi
+
+echo "<insert message that tells you that the package is compiling or something>"
+cargo build --release
+
+echo "Installing binary..."
+sudo install -m 755 target/release/weather-searcher /usr/local/bin/weather-searcher
+
+echo "Done. Now enter weather-searcher in your terminal of choice to run it."
